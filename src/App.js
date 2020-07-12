e38bade09249fa9848c6775dba14fe0dea75f772
Reeman09 <napitreeman09@gmail.com>
2020-07-12 19:59:36 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as ReactBootstrap from "react-bootstrap";
 
-function App() {
+const App = () =>{
+  const students =[
+    {FirstName:"Reeman" , LastName:"Napit", Address:"Kirtipur", PhoneNumber:"9843363069", SchoolName:"Pushpasadan"},
+    {FirstName:"Nikesh" , LastName:"Maharjan", Address:"Panga", PhoneNumber:"559599292962", SchoolName:"Ujjwal"},
+    {FirstName:"Dibesh" , LastName:"Maharjan", Address:"Panga", PhoneNumber:"18484262", SchoolName:"Panga school"},
+    {FirstName:"Aman" , LastName:"Maharjan", Address:"Panga", PhoneNumber:"5848482", SchoolName:"Hill town"},
+    {FirstName:"Dipesh" , LastName:"Maharjan", Address:"Kirtipur", PhoneNumber:"5487484", SchoolName:"Rarahil"},
+  ]
+const renderStudent =(Student, index)=>{
+  return(
+    <tr key={index}>
+      <td>{Student.FirstName}</td>
+      <td>{Student.LastName}</td>
+      <td>{Student.Address}</td>
+      <td>{Student.PhoneNumber}</td>
+      <td>{Student.SchoolName}</td>
+    </tr>
+  )
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactBootstrap.Table striped bordered hover>
+    <thead>
+      <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Address</th>
+        <th>Phone Number</th>
+        <th>School Name </th>
+      </tr>
+    </thead>
+    <tbody>
+     {students.map(renderStudent)}
+    </tbody>
+  </ReactBootstrap.Table>    </div>
   );
 }
 
